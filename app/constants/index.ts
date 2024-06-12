ea5a541e9ee2260f2920cbe46a321dbc16f53413
Sonal Jayasinghe <sonaldanindulk@gmail.com>
2024-06-12 14:21:14 +0530

    updated constants

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -4,46 +4,45 @@ export enum GENDER {
 }
 
 export enum UNIVERSITY {
-    SRI_JAYEWARDENEPURA = 'sri_jayewardenepura',
-    COLOMBO = 'colombo',
-    PERADENIYA = 'peradeniya',
-    KELANIYA = 'kelaniya',
-    MORATUWA = 'moratuwa',
-    JAFFNA = 'jaffna',
-    RUHUNA = 'ruhuna',
-    OPEN_UNIVERSITY = 'open',
-    EASTERN = 'eastern',
-    SOUTH_EASTERN = 'south_eastern',
-    RAJARATA = 'rajarata',
-    SABARAGAMUWA = 'sabaragamuwa',
-    WAYAMBA = 'wayamba',
-    UVA_WELLASSA = 'uva_wellassa',
-    VISUAL_PERFORMING_ARTS = 'visual_performing_arts',
-    GAMPANA_WICKRAMARACHCHI = 'gampaha_wickramarachchi',
-    MORATUWA_TECHNOLOGY = 'moratuwa_technology',
-    VAUNIYA = 'vauniya',
+    SRI_JAYEWARDENEPURA = 'University of Sri Jayewardenepura',
+    COLOMBO = 'University of Colombo',
+    PERADENIYA = 'University of Peradeniya',
+    KELANIYA = 'University of Kelaniya',
+    MORATUWA = 'University of Moratuwa',
+    JAFFNA = 'University of Jaffna',
+    RUHUNA = 'University of Ruhuna',
+    OPEN_UNIVERSITY = 'The Open University of Sri Lanka',
+    EASTERN = 'Eastern University, Sri Lanka',
+    SOUTH_EASTERN = 'South Eastern University of Sri Lanka',
+    RAJARATA = 'Rajarata University of Sri Lanka',
+    SABARAGAMUWA = 'Sabaragamuwa University of Sri Lanka',
+    WAYAMBA = 'Wayamba University of Sri Lanka',
+    UVA_WELLASSA = 'Uva Wellassa University',
+    VISUAL_PERFORMING_ARTS = 'University of the Visual & Performing Arts',
+    GAMPANA_WICKRAMARACHCHI = 'University of the Sri Lanka (Gampaha Wickramarachchi Ayurveda Institute)',
+    MORATUWA_TECHNOLOGY = 'Institute of Technology University of Moratuwa',
+    VAUNIYA = 'University of Vavuniya',
 }
 
 export enum ACADEMICYEAR {
-    Year1 = '1',
-    Year2 = '2',
-    Year3 = '3',
-    Year4 = '4',
-    Year5 = '5',
+    Year1 = '1st Year (22/23)',
+    Year2 = '2nd Year (21/22)',
+    Year3 = '3rd Year (20/21)',
+    Year4 = '4th Year (19/20)',
+    Year5 = '5th Year (18/19)',
 }
 
 export enum FACULTY {
-    MANAGEMENT_STUDIES_AND_COMMERCE = 'Management Studies & Commerce',
-    APPLIED_SCIENCES = 'Applied Sciences',
-    HUMANITIES_AND_SOCIAL_SCIENCES = 'Humanities and Social Sciences',
-    ALLIED_HEALTH_SCIENCES = 'Allied Health Sciences',
-    TECHNOLOGY = 'Technology',
-    ENGINEERING = 'Engineering',
-    MEDICAL_SCIENCE = 'Medical Science'
+    MANAGEMENT_STUDIES_AND_COMMERCE = 'Faculty of Management Studies & Commerce',
+    APPLIED_SCIENCES = 'Faculty of Applied Sciences',
+    HUMANITIES_AND_SOCIAL_SCIENCES = 'Faculty of Humanities and Social Sciences',
+    ALLIED_HEALTH_SCIENCES = 'Faculty of Allied Health Sciences',
+    TECHNOLOGY = 'Faculty of Technology',
+    ENGINEERING = 'Faculty of Engineering',
+    MEDICAL_SCIENCE = 'Faculty of Medical Science'
 }
 
 export enum DEGREE {
-    MANAGEMENT_STUDIES_AND_COMMERCE = 'Management Studies & Commerce',
     ACCOUNTING_SPECIAL = 'B.Sc. Accounting (Special) Degree',
     FINANCE_SPECIAL = 'B.Sc. Finance (Special) Degree',
     BUSINESS_ADMINISTRATION_SPECIAL = 'B.Sc. Business Administration (Special) Degree',
@@ -56,11 +55,13 @@ export enum DEGREE {
     PUBLIC_MANAGEMENT_SPECIAL = 'B.Sc. Public Management (Special) Degree',
     ESTATE_MANAGEMENT_VALUATION_SPECIAL = 'B.Sc. Estate Management and Valuation (Special) Degree',
     COM_SPECIAL = 'B.Com (Special) Degree',
+
     GENERAL_3YEARS = 'B.Sc. (General) Degree (3 years)',
     SPECIAL_4YEARS = 'B.Sc. (Special) Degree (4 years)',
     HONOURS_APPLIED_SCIENCES_4YEARS = 'B.Sc. Honours Degree in Applied Sciences (4 years)',
     FOOD_SCIENCE_TECHNOLOGY_SPECIAL = 'B.Sc. Food Science and Technology (Special) Degree',
     SPORT_SCIENCE_MANAGEMENT_SPECIAL = 'B.Sc. Sport Science and Management (Special) Degree',
+
     BA_GENERAL = 'B.A. (General) Degree',
     BSC_INFORMATION_TECHNOLOGY_SPECIAL = 'B.Sc. Information Technology (Special) Degree',
     BA_GEOGRAPHY_SPECIAL = 'B.A. Geography (Special) Degree',
@@ -88,10 +89,12 @@ export enum DEGREE {
     BA_PHILOSOPHY_PSYCHOLOGY_SPECIAL = 'B.A. Philosophy and Psychology (Special) Degree',
     BA_BUDDHIST_HERITAGE_TOURISM_SPECIAL = 'B.A. Buddhist Heritage & Tourism (Special) Degree',
     BSC_HONOURS_GEO_SPATIAL_TECHNOLOGY = 'B.Sc. Honours in Geo-Spatial Technology',
+
     MEDICAL_LAB_SCIENCES = 'B.Sc. in Medical Laboratory Sciences',
     NURSING = 'B.Sc. in Nursing',
     PHARM_HONS = 'B.Pharm(Hons)',
     OPTOMETRY = 'B.Sc in Optometry',
+
     BIOSYSTEMS_TECHNOLOGY_HONOURS = 'Bachelor of Biosystems Technology (Honours) Degree',
     ENGINEERING_TECHNOLOGY_HONOURS = 'Bachelor of Engineering Technology (Honours) Degree',
     INFORMATION_COMMUNICATION_TECHNOLOGY_HONOURS = 'Bachelor of Information and Communication Technology (Honours) Degree',
@@ -99,6 +102,7 @@ export enum DEGREE {
     CIVIL_ENGINEERING_SPECIAL = 'B.Sc.Civil Engineering (Special) Degree',
     ELECTRICAL_ELECTRONIC_ENGINEERING_SPECIAL = 'B.Sc.Electrical and Electronic Engineering (Special) Degree',
     MECHANICAL_ENGINEERING_SPECIAL = 'B.Sc.Mechanical Engineering (Special) Degree',
+    
     MBBS = 'MBBS',
     HUMAN_BIOLOGY = 'B.Sc. in Human Biology',
 }
