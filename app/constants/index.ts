0e36e9d857b203c7b3778144a46a228a098b2a3f
YasithaRenuk <126389901+YasithaRenuk@users.noreply.github.com>
2024-06-12 10:44:53 +0530

    +add constants

---
+++ app/constants/index.ts
@@ -0,0 +1,16 @@
+export const enum GENDER {
+    MALE = 'M',
+    FEMALE = 'F',
+}
+
+export const enum UNIVERSITY {
+    INTERUNIVERSITY='inter',
+    JAYAWARDANAPURA='japura'
+}
+
+export const enum ACADEMICYEAR {
+    Year1='1',
+    Year2='2',
+    Year3='3',
+    Year4='4',
+}
--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const  connectMongoDB= async ()=>{
     try{
-       await mongoose.connect(process.env.MONGODB_URI);
+       await mongoose.connect(process.env.MONGODB_URI!);
         console.log("Connected to the MongoDB")
     }catch(error){
         console.log(error)
