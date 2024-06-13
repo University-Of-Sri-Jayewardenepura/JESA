4d97cc7a908c81c259084f6a3a4de1d5072b5f8e
Sonal Jayasinghe <sonaldanindulk@gmail.com>
2024-06-14 01:43:54 +0530

    registration page UI fixed

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,5 +1,4 @@
 import { Metadata } from "next/types";
-import RegisterForm from "./register-form";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
@@ -11,13 +10,26 @@ export const metadata: Metadata = {
 
 export default function Register() {
   return (
-    <div className="flex flex-col items-center min-h-screen pt-32 w-full">
-        <h1> Jessa 24 Registration </h1>
-        <h2 className=" text-slate-300"> Please select the category to proceed with the form </h2>
-        <div className=" flex flex-col">
-          <Button className="mt-4"> <Link rel="preload" href="/register/internal"> Students of University of Sri Jayewardenepura </Link></Button>
-          <Button className="mt-4"> <Link rel="preload" href="/register/external"> Students of Other State Universities </Link></Button>
-        </div>
+    <div className="flex flex-col items-center min-h-screen pt-32 w-full space-y-3">
+      <h1 className=" text-4xl">JESA 2024 REGISTRATION</h1>
+      <h2 className="text-slate-400">
+        Please select the category to proceed with the form
+      </h2>
+    
+      <div className="flex flex-row justify-center space-x-4 items-center h-20">
+        <Link rel="preload" href="/register/internal">
+          <Button className="mt-4 px-6 py-2 shadow-md flex-1 h-24 w-64">
+            Students of <br />
+            University of Sri Jayewardenepura
+          </Button>
+        </Link>
+        <Link rel="preload" href="/register/external">
+          <Button className="mt-4 px-6 py-2 shadow-md flex-1 h-24 w-64">
+            Students of <br />
+            Other State Universities
+          </Button>
+        </Link>
+      </div>
     </div>
   );
 }
