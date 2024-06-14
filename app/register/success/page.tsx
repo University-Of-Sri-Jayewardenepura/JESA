4afffb5ad5d21da47d26244dcaefd8bcfe0efa2c
Sonal Jayasinghe <sonaldanindulk@gmail.com>
2024-06-14 13:33:11 +0530

    success page ui updated

--- app/register/success/page.tsx
+++ app/register/success/page.tsx
@@ -4,9 +4,12 @@ import React from 'react'
 
 const SuccessPage = () => {
   return (
-    <div className="flex flex-col items-center min-h-screen pt-32 w-full">
-    <h1> Jessa 24 Registration </h1>
-    <h2 className=" text-slate-300"> Hoooooreh! Your registration form was recieved! </h2>
+    <div className="flex flex-col items-center min-h-screen pt-32 w-full space-y-3">
+    <h1 className=" sm:text-5xl text-4xl text-center pl-3 pr-3">JESA 2024 REGISTRATION</h1>
+    <h2 className="text-slate-300 text-2xl text-center pl-3 pr-3">
+      Your registration form was recieved. Thank you!
+    </h2>
+  
     <div className=" flex flex-col">
       <Button className="mt-4"> <Link href="/"> Back to Home </Link></Button>
     </div>
