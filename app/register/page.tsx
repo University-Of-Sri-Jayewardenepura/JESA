3c98c4b388ba0cd563b4f6371f609b204fe7a7db
Sonal Jayasinghe <sonaldanindulk@gmail.com>
2024-06-14 09:03:34 +0530

    main registration page ui optimized

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -11,20 +11,20 @@ export const metadata: Metadata = {
 export default function Register() {
   return (
     <div className="flex flex-col items-center min-h-screen pt-32 w-full space-y-3">
-      <h1 className=" text-4xl">JESA 2024 REGISTRATION</h1>
-      <h2 className="text-slate-400">
+      <h1 className=" sm:text-5xl text-4xl text-center pl-3 pr-3">JESA 2024 REGISTRATION</h1>
+      <h2 className="text-slate-400 text-center pl-3 pr-3">
         Please select the category to proceed with the form
       </h2>
     
-      <div className="flex flex-row justify-center space-x-4 items-center h-20">
+      <div className="flex flex-row justify-center items-center h-20 flex-wrap">
         <Link rel="preload" href="/register/internal">
-          <Button className="mt-4 px-6 py-2 shadow-md flex-1 h-24 w-64">
+          <Button className="m-4 px-6 py-2 shadow-md flex-1 h-24 w-64">
             Students of <br />
             University of Sri Jayewardenepura
           </Button>
         </Link>
         <Link rel="preload" href="/register/external">
-          <Button className="mt-4 px-6 py-2 shadow-md flex-1 h-24 w-64">
+          <Button className="m-4 px-6 py-2 shadow-md flex-1 h-24 w-64">
             Students of <br />
             Other State Universities
           </Button>
