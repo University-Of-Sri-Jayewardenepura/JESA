2df78f6c537879bb5e25a188a8176e6bc6cdbbc7
pruthivithejan <pruthivithejan.code@gmail.com>
2024-06-17 18:08:15 +0530

    Fixed button width

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
 
 export default function Register() {
   return (
-    <div className="flex flex-col items-center min-h-screen pt-32 w-full space-y-3">
+    <div className="flex flex-col items-center min-h-screen pt-32 space-y-3">
       <h1
         className={`mt-8 bg-gradient-to-br from-amber-300 to-amber-500 bg-clip-text text-center text-4xl md:text-6xl lg:text-7xl font-bold uppercase tracking-tight text-transparent  ${zodiak.className}`}
       >
@@ -20,28 +20,21 @@ export default function Register() {
       <h2 className="text-sm md:text-lg text-white/75 text-center px-3">
         Please select the category to proceed with the form
       </h2>
-      <div className="flex flex-col justify-center items-center pt-[2rem] space-y-8">
-        <Link
-          rel="preload"
-          href="/register/internal"
-          className="w-full md:w-[350px]"
-        >
-          <Button className="font-bold shadow-md w-full py-6">
+      <div className="flex flex-col justify-center items-center pt-[2rem] space-y-8 w-[18rem] md:w-[28rem]">
+        <Link rel="preload" href="/register/internal" className="w-full">
+          <Button className="font-bold shadow-md w-full py-11">
             <span className="bg-gradient-to-b from-background to-emerald-950 bg-clip-text text-transparent">
               Students of <br />
               University of Sri Jayewardenepura
             </span>
           </Button>
         </Link>
-        <Link
-          rel="preload"
-          href="/register/external"
-          className="w-full md:w-[350px]"
-        >
-          <Button className="font-bold shadow-md w-full py-6 h-[65px]">
+        <Link rel="preload" href="/register/external" className="w-full">
+          <Button className="font-bold shadow-md w-full py-11 h-[65px]">
             <span className="bg-gradient-to-b from-background to-emerald-950 bg-clip-text text-transparent">
               Students of <br />
-              Other State Universities<br/>
+              Other State Universities
+              <br />
               (For Best Innovator Award Only)
             </span>
           </Button>
