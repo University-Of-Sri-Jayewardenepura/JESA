d558d2fc450aa488cef6f7b65238b1fe3ab53c73
WarunaUdara <warunaudarasam2003@gmail.com>
2026-07-13 23:03:43 +0530

    feat(seo): configure metadata Base and OpenGraph/Twitter card using opengraph-image.webp

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,32 @@ export const metadata: Metadata = {
 	title: "JESA 2026 | J'pura Employability Skills Awards",
 	description:
 		"Official 2026 JESA aka J'pura Employability Skills Awards, the ultimate platform for honoring the accomplishments of young talents.",
+	metadataBase: new URL("https://jesa.usjp.ac.lk"),
+	openGraph: {
+		title: "JESA 2026 | J'pura Employability Skills Awards",
+		description:
+			"Official 2026 JESA aka J'pura Employability Skills Awards, the ultimate platform for honoring the accomplishments of young talents.",
+		url: "https://jesa.usjp.ac.lk",
+		siteName: "JESA 2026",
+		images: [
+			{
+				url: "/images/opengraph-image.webp",
+				width: 1200,
+				height: 630,
+				alt: "JESA 2026 | J'pura Employability Skills Awards",
+				type: "image/webp",
+			},
+		],
+		locale: "en_US",
+		type: "website",
+	},
+	twitter: {
+		card: "summary_large_image",
+		title: "JESA 2026 | J'pura Employability Skills Awards",
+		description:
+			"Official 2026 JESA aka J'pura Employability Skills Awards, the ultimate platform for honoring the accomplishments of young talents.",
+		images: ["/images/opengraph-image.webp"],
+	},
 };
 
 export default function RootLayout({
