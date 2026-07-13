93514c5e4ae98a21b4d3d5e104f9e9ff190ac83e
WarunaUdara <warunaudarasam2003@gmail.com>
2026-07-14 00:00:26 +0530

    feat(layout): disable top announcement banner and adjust header layout to top-0

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,4 +1,4 @@
-import Announcement from "@/components/annoucement";
+// import Announcement from "@/components/annoucement";
 import Footer from "@/components/footer";
 import Nav from "@/components/nav";
 
@@ -12,8 +12,8 @@ export default function SiteLayout({
 			className="flex min-w-[340px] flex-col overflow-hidden font-sans text-slate-200 antialiased"
 			style={{ background: "var(--background-gradient)" }}
 		>
-			<header className="fixed top-9 right-0 left-0 z-50 flex justify-center bg-background px-5 px-safe pt-safe lg:h-16">
-				<Announcement />
+			<header className="fixed top-0 right-0 left-0 z-50 flex justify-center bg-background px-5 px-safe pt-safe lg:h-16">
+				{/* <Announcement /> */}
 				<Nav />
 			</header>
 			<div className="min-h-screen">{children}</div>
