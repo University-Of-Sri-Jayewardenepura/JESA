82d43c410dfc6cd5ce360d44eb1b45f277aff8d0
DESHAN K.D.M <130528796+DHARAKA-METH@users.noreply.github.com>
2026-09-09 16:29:18 +0530

    feat:registration closing

--- components/annoucement.tsx
+++ components/annoucement.tsx
@@ -10,7 +10,7 @@ const Announcement: React.FC = () => {
 			className="fixed top-0 z-50 flex h-9 w-full items-center justify-center gap-x-4 overflow-hidden border-gradient-to-r border-b from-slate-700 via-slate-500 to-slate-700 lg:px-safe"
 			style={{ background: "var(--gradient-annoucement)" }}
 		>
-			<div className="flex items-center gap-x-3.5">
+			{/* <div className="flex items-center gap-x-3.5">
 				<p className="space-x-2 text-sm leading-snug tracking-tighter">
 					JESA 2026 Registration are open &nbsp;
 					<span className="opacity-30">|</span>
@@ -23,7 +23,7 @@ const Announcement: React.FC = () => {
 						<ArrowRight className="inline h-4 w-4 transition-transform duration-200 group-hover:translate-x-1 group-hover:text-slate-300" />
 					</Link>
 				</p>
-			</div>
+			</div> */}
 		</div>
 	);
 };
